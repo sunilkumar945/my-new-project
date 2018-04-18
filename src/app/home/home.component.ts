9f8625bf35864786e2c4d8685eebefead6c026e7
Sunil Kumar K S <sunilkumar945@gmail.com>
2018-04-18 18:05:24 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 import { DataService } from '../data.service';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
